f18f0965bdad28ace8e4a06b90fd7746d1cdb110
https://github.com/MrBrocleherst/Lab1/ <edemseitasanov@gmail.com>
2025-02-13 12:30:05 +0200

    поміняв println, поміняв методи в ютілс класі та написав юніт тести до кожного класу і ще трохи підкоригував щоб виглядало адекватно

--- src/test/java/org/example/VillageTest.java
+++ src/test/java/org/example/VillageTest.java
@@ -8,7 +8,6 @@ class VillageTest {
     @Test
     void testSetName() {
         Village village = new Village("Зелене", 500, 20.5);
-        assertEquals("Зелене", village.getName());
 
         village.setName("Смарагдове");
         assertEquals("Смарагдове", village.getName());
@@ -17,7 +16,6 @@ class VillageTest {
     @Test
     void testSetPopulation() {
         Village village = new Village("Зелене", 500, 20.5);
-        assertEquals(500, village.getPopulation());
 
         village.setPopulation(600);
         assertEquals(600, village.getPopulation());
@@ -26,7 +24,6 @@ class VillageTest {
     @Test
     void testSetArea() {
         Village village = new Village("Зелене", 500, 20.5);
-        assertEquals(20.5, village.getArea());
 
         village.setArea(25.0);
         assertEquals(25.0, village.getArea());
@@ -43,8 +40,6 @@ class VillageTest {
     void testIllegalArgumentExceptionOnInvalidPopulation() {
         Village village = new Village("Зелене", 500, 20.5);
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            village.setPopulation(-100);
-        });
+        assertThrows(IllegalArgumentException.class, () -> village.setPopulation(-100));
     }
 }
